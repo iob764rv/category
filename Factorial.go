c197d5040778ab0be16ac48348a7ad09bb90a702
iob764rv <iic287m@gmail.com>
2017-08-30 18:07:35 -0400

    Create Factorial.go

---
+++ Factorial.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func factorial(a int) int{
+	var result int;
+	if a==0{
+		return 1;
+	}else{
+		result=a*factorial(a-1)
+		return result
+	}
+}
+
+func main() {
+//	var input int;
+	
+	fmt.Println("Factorial is",factorial(4));
+}
+
+//factorials
