0e93bee981166cc34071fca16fb20fb472478dd4
iob764rv <iic287m@gmail.com>
2017-09-15 22:44:48 -0400

    Update StringOutput.go

--- StringOutput.go
+++ StringOutput.go
@@ -11,6 +11,13 @@ func main(){
 
 	fmt.Println("StringS")
 	fmt.Println(strings.ToUpper("StringS"))
+	
+	reader :=bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text")
+	line, _ := reader.ReadString('\n')
+	
+	fmt.Println(line)
+	fmt.Println(strings.ToUpper(line))
 
 
 }
